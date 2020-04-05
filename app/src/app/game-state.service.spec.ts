6c79856d1a81228537f4dc2e92598bf8daea8bca
Tyler Mumford <tyler.mumford@icloud.com>
2020-04-05 02:22:20 -0600

    Add lots of behavior for scoring.

--- app/src/app/game-state.service.spec.ts
+++ app/src/app/game-state.service.spec.ts
@@ -12,45 +12,127 @@ describe('GameStateService', () => {
     service = TestBed.inject(GameStateService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  describe('Preparing a game', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
 
-  it('should start with no players', () => {
-    expect(service.playerCount).toBe(0);
+    it('should start with no players', () => {
+      expect(service.playerCount).toBe(0);
+    })
+
+    it('should start in the "Preparing" status', () => {
+      expect(service.status).toBe(GameStatus.Preparing);
+    })
+
+    it('should allow a player to be added', () => {
+      expect(() => service.addPlayer("Player 1")).not.toThrow()
+      expect(service.playerCount).toBe(1);
+    })
+
+    it('should allow three players to be added', () => {
+      try {
+        service.addPlayer("Player 1")
+        service.addPlayer("Player 2")
+        service.addPlayer("Player 3")
+      } catch (e) {
+        fail("Could not add player")
+      }
+      expect(service.playerCount).toBe(3);
+    })
+
+    it('should not allow the game to be started with 0 players', () => {
+      expect(service.playerCount).toBe(0)
+      expect(service.startPlaying).toThrow()
+      expect(service.status).toBe(GameStatus.Preparing)
+    })
+
+    it('should allow the game to be started with at least one player', () => {
+      expect(() => {
+        service.addPlayer('Player 1')
+        service.startPlaying()
+      }).not.toThrow()
+      expect(service.status).toBe(GameStatus.Playing)
+    })
   })
 
-  it('should start in the "Preparing" status', () => {
-    expect(service.status).toBe(GameStatus.Preparing);
-  })
+  describe('Starting the first round', () => {
+    const p1 = 'Player One', p2 = 'Player Two'
 
-  it('should allow a player to be added', () => {
-    expect(() => service.addPlayer("Player 1")).not.toThrow()
-    expect(service.playerCount).toBe(1);
-  })
-
-  it('should allow three players to be added', () => {
-    try {
-      service.addPlayer("Player 1")
-      service.addPlayer("Player 2")
-      service.addPlayer("Player 3")
-    } catch (e) {
-      fail("Could not add player")
-    }
-    expect(service.playerCount).toBe(3);
-  })
-
-  it('should not allow the game to be started with 0 players', () => {
-    expect(service.playerCount).toBe(0)
-    expect(service.startPlaying).toThrow()
-    expect(service.status).toBe(GameStatus.Preparing)
-  })
-
-  it('should allow the game to be started with at least one player', () => {
-    expect(() => {
-      service.addPlayer('Player 1')
+    beforeEach(() => {
+      service.addPlayer(p1)
+      service.addPlayer(p2)
       service.startPlaying()
-    }).not.toThrow()
-    expect(service.status).toBe(GameStatus.Playing)
+    })
+
+    it('should start at zero score', () => {
+      expect(service.scores).toBeTruthy()
+      expect(service.scores).toEqual({
+        [p1]: 0,
+        [p2]: 0
+      })
+    })
+
+    it('should start on round one', () => {
+      expect(service.currentRound).toBe(1)
+    })
+
+    it('should allow the rolls to be updated', () => {
+      expect(() => service.setRollsForRound(1, [])).not.toThrow()
+      expect(() => service.setRollsForRound(1, [2])).not.toThrow()
+      expect(() => service.setRollsForRound(1, [3])).not.toThrow()
+      expect(() => service.setRollsForRound(1, [7])).not.toThrow()
+      expect(() => service.setRollsForRound(1, [12])).not.toThrow()
+    })
+
+    it('should allow a D for rolling doubles after three rolls', () => {
+      expect(() => service.setRollsForRound(1, [5, 5, 5, 'D'])).not.toThrow()
+    })
+
+    it('should reject impossible roll numbers', () => {
+      expect(() => service.setRollsForRound(1, [0])).toThrow()
+      expect(() => service.setRollsForRound(1, [1])).toThrow()
+      expect(() => service.setRollsForRound(1, [-1])).toThrow()
+      expect(() => service.setRollsForRound(1, [13])).toThrow()
+      expect(() => service.setRollsForRound(1, [70])).toThrow()
+    })
+
+    describe('Points at stake', () => {
+      it('should start at zero', () => {
+        expect(service.pointsAtStakeInRound(1)).toBe(0)
+      })
+
+      it('should sum the basic dice rolls', () => {
+        service.setRollsForRound(1, [3])
+        expect(service.pointsAtStakeInRound(1)).toBe(3)
+        service.setRollsForRound(1, [3, 5])
+        expect(service.pointsAtStakeInRound(1)).toBe(8)
+        service.setRollsForRound(1, [3, 5, 10, 11, 3])
+        expect(service.pointsAtStakeInRound(1)).toBe(32)
+      })
+
+      it('should score seventy in the first three rolls', () => {
+        service.setRollsForRound(1, [7])
+        expect(service.pointsAtStakeInRound(1)).toBe(70)
+        service.setRollsForRound(1, [5, 7])
+        expect(service.pointsAtStakeInRound(1)).toBe(75)
+        service.setRollsForRound(1, [5, 5, 7])
+        expect(service.pointsAtStakeInRound(1)).toBe(80)
+      })
+    })
+
+    describe('Players going out', () => {
+      it('should reject going out "in the future"', () => {
+        expect(() => service.setPlayerOut(p1, 1, 0)).toThrow()
+        service.setRollsForRound(1, [3, 5, 9])
+        expect(() => service.setPlayerOut(p1, 1, 3)).toThrow()
+        expect(() => service.setPlayerOut(p1, 2, 0)).toThrow()
+      })
+
+      it('should accept going out after three rolls', () => {
+        service.setRollsForRound(1, [3, 5, 9])
+        expect(() => service.setPlayerOut(p1, 1, 2)).not.toThrow()
+      })
+    })
   })
 });
--- app/src/app/game-state.service.ts
+++ app/src/app/game-state.service.ts
@@ -8,9 +8,12 @@ export class GameStateService {
 
   get playerCount() { return this.players.length }
   get status() { return this._status }
+  get scores() { return this._scores }
+  get currentRound() { return 1 }
 
   addPlayer(name: string) {
     this.players.push(name)
+    this.scores[name] = 0
   }
 
   startPlaying() {
@@ -21,8 +24,48 @@ export class GameStateService {
     }
   }
 
-  constructor() { }
+  pointsAtStakeInRound(round: number) {
+    const rolls = this._roundRolls[round];
+    if (rolls === undefined)
+      return 0
+
+    return rolls
+      .map(n => (n === 7) ? 70 : n)
+      .reduce((prev, curr, i) => {
+        const prevN = prev as number
+        if (curr === 'D')
+          return prevN * 2
+        else
+          return prevN + curr
+      })
+  }
+
+  setRollsForRound(round: number, rolls: Roll[]) {
+    rolls.forEach(this.rejectBadRollNumber)
+    this._roundRolls[round] = rolls
+  }
+
+  setPlayerOut(playerName: string, round: number, withRollIndex: number) {
+    if (withRollIndex > this._roundRolls[round].length - 1)
+      throw new Error("Can't go out at that time")
+  }
 
   private _status = GameStatus.Preparing
   private players: string[] = []
+  private _scores: {[player: string]: number} = {}
+
+  private _roundRolls: RoundRolls = {}
+
+  private rejectBadRollNumber(attemptedRoll: number) {
+    const r = attemptedRoll;
+    if (r < 2 || r > 12) {
+      throw new Error(`It's impossible to roll a ${r} with two dice.`)
+    }
+  }
+}
+
+type Roll = number | 'D'
+
+interface RoundRolls {
+  [round: number]: Roll[]
 }
