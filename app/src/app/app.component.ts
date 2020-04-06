5b88ffdf07a52fb5dbcbad35c1db7cb99a5e24f3
Tyler Mumford <tyler.mumford@icloud.com>
2020-04-06 00:09:14 -0600

    On start playing, transition to game UI.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GameStateService } from './game-state.service';
+import { GameStatus } from './GameStatus';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Stock Market Game';
+  title = 'Stock Market Game'
+
+  get isStatusPreparing() { return this.game.status === GameStatus.Preparing }
+  get isStatusPlaying() { return this.game.status === GameStatus.Playing }
+
+  constructor(private game: GameStateService) {}
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameSetupComponent } from './game-setup/game-setup.component';
+import { GameUiComponent } from './game-ui/game-ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameSetupComponent
+    GameSetupComponent,
+    GameUiComponent
   ],
   imports: [
     BrowserModule,
