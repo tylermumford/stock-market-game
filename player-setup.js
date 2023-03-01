d6e5c24e8f88c922398654b2febb0fc2eabdb48f
Tyler Mumford <tyler.mumford@icloud.com>
2023-02-28 23:58:31 -0700

    Move setup HTML for faster load

--- player-setup.js
+++ player-setup.js
@@ -1,6 +1,5 @@
 export function init(selector) {
   let element = document.querySelector(selector);
-  element.innerHTML = html;
   element.addEventListener("keyup", handleEnterKey);
 
   let button = document.querySelector("#start-game");
@@ -38,40 +37,3 @@ function tryStartGame() {
     alert(e)
   }
 }
-
-const html = `
-<h2>Game Setup</h2>
-
-<p>
-  <label>Player 1: <input type="text" autofocus></label>
-</p>
-<p>
-  <label>Player 2: <input type="text"></label>
-</p>
-<p>
-  <label>Player 3: <input type="text"></label>
-</p>
-<p>
-  <label>Player 4: <input type="text"></label>
-</p>
-<p>
-  <label>Player 5: <input type="text"></label>
-</p>
-<p>
-  <label>Player 6: <input type="text"></label>
-</p>
-<p>
-  <label>Player 7: <input type="text"></label>
-</p>
-<p>
-  <label>Player 8: <input type="text"></label>
-</p>
-<p>
-  <label>Player 9: <input type="text"></label>
-</p>
-<p>
-  <label>Player 10: <input type="text"></label>
-</p>
-
-<button id="start-game">Start game</button>
-`
